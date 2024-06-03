c53b38f1b58581114d126fecdd2930bb59339407
ayberkyonar <302155034@student.rocmondriaan.nl>
2024-06-03 21:18:32 +0200

    test

--- tests/Test2.java
+++ tests/Test2.java
@@ -10,9 +10,10 @@ public class Test2 {
         // Arrange
         Sprint sprint = new Sprint("Test Sprint");
         Taak taak = new Programmeer("Test Taak", "Dit is een test taak", "Klaar", new Date(), "JavaScript", 4);
+        Gebruiker gebruiker = new Tester("Test Tester", 4);
 
         // Act
-        //sprint.addTaak(taak);
+        sprint.addTaak(taak, gebruiker);
 
         // Assert
         Assertions.assertTrue(sprint.getTaken().contains(taak));
