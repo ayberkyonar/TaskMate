bd404198f0b421a11fdbfe5ba2a9006c29d0d8cc
ayberkyonar <yonarayberk@gmail.com>
2024-06-04 10:56:55 +0200

    e

--- src/model/TaakMethode.java
+++ src/model/TaakMethode.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class TaakMethode {
 
-    public void load(Taak taken) {
-        System.out.println("Naam: " + taken.getNaam());
-        System.out.println("Beschrijving " + taken.getBeschrijving());
-        System.out.println("Status: " + taken.getStatus());
-        System.out.println("Storypoints: " + taken.getTaakPunten());
-        System.out.println("Datum aangemaakt: " + taken.getDatumTijd());
-        System.out.println("Type taak: " + taken.getTaakType());
-        taken.printSpecifiekeInformatie();
+    public void load(Taak taak) {
+        System.out.println("Naam: " + taak.getNaam());
+        System.out.println("Beschrijving " + taak.getBeschrijving());
+        System.out.println("Status: " + taak.getStatus());
+        System.out.println("Storypoints: " + taak.getTaakPunten());
+        System.out.println("Datum aangemaakt: " + taak.getDatumTijd());
+        System.out.println("Type taak: " + taak.getTaakType());
+        taak.printSpecifiekeInformatie();
         System.out.println();
     }
 }
